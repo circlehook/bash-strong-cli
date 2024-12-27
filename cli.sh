8b36b964d1e1021ce9e771f0ba821a7d5c1d0174
Dmitry <dvinichenko86@gmail.com>
2024-12-27 10:31:47 +0200

    fix cli.sh

--- cli.sh
+++ cli.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 wget -q -O - https://raw.githubusercontent.com/circlehook/bash-strong-cli/refs/heads/main/strong_aliases > ~/.strong_aliases
 grep -q "strong_aliases" ~/.bashrc || echo "[ -f ~/.strong_aliases ] && . ~/.strong_aliases" >> ~/.bashrc
